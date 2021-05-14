3fb352e66770bd56e715869462d30d2da452696c
Option-Panda <optionpanda@protonmail.com>
2021-05-15 00:04:50 +0800

    check confirm

--- main.go
+++ main.go
@@ -69,13 +69,23 @@ func main() {
 			log.Println("Provider:", provider)
 			log.Println("GasLimit:", gasLimit)
 
-			tryUpdate(provider, priv, contractAddress, gasLimit)
+			tryUpdate(provider, priv, contractAddress, gasLimit, common.Big0)
 			ticker := time.NewTicker(1 * time.Minute)
+
+			lastUpdateTime := common.Big0
+
+		WAIT_EXPIRY:
 			for {
 				select {
 				case <-ticker.C:
 					// fail-retry
-					for !tryUpdate(provider, priv, contractAddress, gasLimit) {
+					for {
+						succ, updateTime := tryUpdate(provider, priv, contractAddress, gasLimit, lastUpdateTime)
+						if succ {
+							lastUpdateTime = updateTime
+							continue WAIT_EXPIRY
+						}
+
 						<-time.After(2 * time.Second)
 					}
 				}
@@ -90,46 +100,52 @@ func main() {
 
 }
 
-func tryUpdate(provider string, key *ecdsa.PrivateKey, address common.Address, gasLimit int) bool {
+func tryUpdate(provider string, key *ecdsa.PrivateKey, address common.Address, gasLimit int, lastUpdateTime *big.Int) (bool, *big.Int) {
 	// create connection
 	client, err := ethclient.Dial(provider)
 	if err != nil {
 		log.Printf("PandaKeeper: connection to  %s failed, reason: %s", provider, err)
-		return false
+		return false, common.Big0
 	}
 	defer client.Close()
 
 	instance, err := NewAggregateUpdater(address, client)
 	if err != nil {
 		log.Println("PandaKeeper: NewAggregateUpdater failed:", err)
-		return false
+		return false, common.Big0
 	}
 
 	// query next update time
 	updateTime, err := instance.GetNextUpdateTime(nil)
 	if err != nil {
 		log.Println("PandaKeeper: GetNextUpdateTime() failed:", err)
-		return false
+		return false, common.Big0
 	}
 
 	log.Printf("PandaKeeper: Next Update:%s", time.Unix(updateTime.Int64(), 0))
 
+	// check confirmation
+	if lastUpdateTime.Cmp(updateTime) == 0 {
+		log.Println("PandaKeeper: still not confirmed:", lastUpdateTime, updateTime)
+		return true, updateTime
+	}
+
 	// still not expired
 	if time.Now().Unix() < updateTime.Int64() {
-		return false
+		return false, common.Big0
 	}
 
 	// query gas price & nonce
 	nonce, err := client.PendingNonceAt(context.Background(), crypto.PubkeyToAddress(key.PublicKey))
 	if err != nil {
 		log.Println("PandaKeeper: client.PendingNonceAt() failed:", err)
-		return false
+		return false, common.Big0
 	}
 
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Println("PandaKeeper: client.SuggestGasPrice() failed:", err)
-		return false
+		return false, common.Big0
 	}
 
 	// create transactor
@@ -141,9 +157,9 @@ func tryUpdate(provider string, key *ecdsa.PrivateKey, address common.Address, g
 	tx, err := instance.Update(auth)
 	if err != nil {
 		log.Println("PandaKeeper: update transaction failed:", err)
-		return false
+		return false, common.Big0
 	}
 
 	log.Println("PandaKeeper: update transaction sent:", tx.Hash().Hex())
-	return true
+	return true, updateTime
 }
